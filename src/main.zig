8da20520785c2122519bd95d2ec7262d8a8da186
eswaidner <eswaidner2@gmail.com>
2025-08-01 15:55:05 -0600

    fixed isometric grid system

--- src/main.zig
+++ src/main.zig
@@ -1,8 +1,15 @@
 const std = @import("std");
 const rl = @import("raylib");
+const Vector2 = rl.Vector2;
+const Matrix = rl.Matrix;
 
 const TILE_SIZE: f32 = 32;
+const HALF_TILE_SIZE: f32 = TILE_SIZE / 2;
+
 const MAP_SIZE: f32 = 7;
+var SCALE: f32 = 4;
+
+var hoveredTile: ?Vector2 = null;
 
 pub fn main() !void {
     const screenWidth = 1920;
@@ -16,14 +23,16 @@ pub fn main() !void {
     const cubeTexture = try rl.loadTexture("./assets/sprites/cube.png");
 
     while (!rl.windowShouldClose()) {
-        const mousePos = rl.getMousePosition();
-        const gridCell = screenToGrid(mousePos.x, mousePos.y);
-        std.log.debug("{d}, {d}", .{ gridCell.x, gridCell.y });
+        hoveredTile = screenToIso(rl.getMousePosition());
+        // std.log.debug("{d}, {d}", .{ hoveredTile.x, hoveredTile.y });
 
         rl.beginDrawing();
         defer rl.endDrawing();
 
         drawGrid(cubeTexture);
+
+        // screen center marker
+        // rl.drawCircle(@divFloor(rl.getScreenWidth(), 2), @divFloor(rl.getScreenHeight(), 2), 4, .white);
 
         rl.clearBackground(.black);
     }
@@ -32,41 +41,66 @@ pub fn main() !void {
 fn drawGrid(tile_sprite: rl.Texture2D) void {
     for (0..MAP_SIZE) |y| {
         for (0..MAP_SIZE) |x| {
-            const x_f: f32 = @floatFromInt(x);
-            const y_f: f32 = @floatFromInt(y);
+            const pos = Vector2{ .x = @floatFromInt(x), .y = @floatFromInt(y) };
+            const screen_pos = isoToScreen(pos);
 
-            const screen_pos = gridToScreen(x_f, y_f);
+            const src: rl.Rectangle = .{ .x = 0, .y = 0, .width = TILE_SIZE, .height = TILE_SIZE };
+            var dest: rl.Rectangle = .{ .x = screen_pos.x, .y = screen_pos.y, .width = TILE_SIZE * SCALE, .height = TILE_SIZE * SCALE };
 
-            const src: rl.Rectangle = .{ .x = 0, .y = 0, .width = 32, .height = 32 };
-            const dest: rl.Rectangle = .{ .x = screen_pos.x, .y = screen_pos.y, .width = 32 * 4, .height = 32 * 4 };
+            if (hoveredTile) |t| {
+                if (t.x == @as(f32, @floatFromInt(x)) and t.y == @as(f32, @floatFromInt(y))) {
+                    dest.y -= SCALE * 2;
+                }
+            }
 
             rl.drawTexturePro(tile_sprite, src, dest, .{ .x = 0, .y = 0 }, 0, .white);
         }
     }
 }
 
-fn gridToScreen(x: f32, y: f32) rl.Vector2 {
-    const i: rl.Vector2 = .{ .x = 1 * TILE_SIZE / 2, .y = 0.5 * TILE_SIZE / 2 };
-    const j: rl.Vector2 = .{ .x = -1 * TILE_SIZE / 2, .y = 0.5 * TILE_SIZE / 2 };
+const I = Vector2{ .x = 1, .y = 0.5 };
+const J = Vector2{ .x = -1, .y = 0.5 };
+const IJ_DET = 1 / (I.x * J.y - J.x * I.y);
+const I_INV = Vector2{ .x = J.y, .y = -I.y };
+const J_INV = Vector2{ .x = -J.x, .y = I.x };
 
-    const width: f32 = @floatFromInt(rl.getScreenWidth());
-    const height: f32 = @floatFromInt(rl.getScreenHeight());
+fn screenToIso(screen: Vector2) Vector2 {
+    var iso = screen.subtract(halfScreenSize());
 
-    const offset: rl.Vector2 = .{ .x = (width / 2) - (TILE_SIZE / 2 * 4), .y = height / 2 - (MAP_SIZE * TILE_SIZE) };
-    return i.scale(x).add(j.scale(y)).scale(4).add(offset);
+    iso = iso.scale(IJ_DET);
+    iso = I_INV.scale(iso.x).add(J_INV.scale(iso.y));
+    iso = iso.scale(1 / SCALE / HALF_TILE_SIZE);
+
+    iso.x = @floor(iso.x + MAP_SIZE / 2);
+    iso.y = @floor(iso.y + MAP_SIZE / 2);
+
+    return iso;
 }
 
-fn screenToGrid(x: f32, y: f32) rl.Vector2 {
-    const i: rl.Vector2 = .{ .x = 1 * TILE_SIZE / 2, .y = 0.5 * TILE_SIZE / 2 };
-    const j: rl.Vector2 = .{ .x = -1 * TILE_SIZE / 2, .y = 0.5 * TILE_SIZE / 2 };
+fn isoToScreen(iso: Vector2) Vector2 {
+    var screen = I.scale(iso.x).add(J.scale(iso.y));
 
-    const det = 1 / (i.x * j.y - j.x * i.y);
-    const i_inv: rl.Vector2 = (rl.Vector2{ .x = j.y, .y = -i.y }).scale(det);
-    const j_inv: rl.Vector2 = (rl.Vector2{ .x = -j.x, .y = i.x }).scale(det);
+    // offset origin to screen center
+    screen.x -= 1;
+    screen.y -= MAP_SIZE / 2;
 
-    //TODO derive offset from screen dimensions?
+    screen = screen.scale(SCALE * HALF_TILE_SIZE).add(halfScreenSize());
 
-    const result = i_inv.scale(x).add(j_inv.scale(y)).scale(0.25).add(.{ .x = -12.5, .y = 2.5 });
+    return screen;
+}
 
-    return .{ .x = @floor(result.x), .y = @floor(result.y) };
+fn screenSize() Vector2 {
+    return .{ .x = @floatFromInt(rl.getScreenWidth()), .y = @floatFromInt(rl.getScreenHeight()) };
+}
+
+fn halfScreenSize() Vector2 {
+    return screenSize().scale(0.5);
+}
+
+fn degrees(rad: f32) f32 {
+    return rad * 180 / std.math.pi;
+}
+
+fn radians(deg: f32) f32 {
+    return deg * std.math.pi / 180;
 }
